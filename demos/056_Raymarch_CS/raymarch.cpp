177ff80039b0577cebaf86077b35395c51572236
Alexander Foksha <AFoksha@luxoft.com>
2017-04-26 18:15:32 +0200

    demo 056 :: Raymarch using compute shader began

---
+++ demos/056_Raymarch_CS/raymarch.cpp
@@ -0,0 +1,142 @@
+//========================================================================================================================================================================================================================
+// DEMO 056 : Ray Marching with Compute Shader
+//========================================================================================================================================================================================================================
+#define GLM_FORCE_RADIANS 
+#define GLM_FORCE_NO_CTOR_INIT
+ 
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include "log.hpp"
+#include "constants.hpp"
+#include "gl_info.hpp"
+#include "glfw_window.hpp"
+#include "shader.hpp"
+#include "camera.hpp"
+#include "glsl_noise.hpp"
+#include "image.hpp"
+
+struct demo_window_t : public glfw_window_t
+{
+    camera_t camera;
+
+    bool hell = true;
+
+    demo_window_t(const char* title, int glfw_samples, int version_major, int version_minor, int res_x, int res_y, bool fullscreen = true)
+        : glfw_window_t(title, glfw_samples, version_major, version_minor, res_x, res_y, fullscreen /*, true */)
+    {
+        gl_info::dump(OPENGL_BASIC_INFO | OPENGL_EXTENSIONS_INFO);
+        camera.infinite_perspective(constants::two_pi / 6.0f, aspect(), 0.5f);
+    }
+
+    //===================================================================================================================================================================================================================
+    // mouse handlers
+    //===================================================================================================================================================================================================================
+    void on_key(int key, int scancode, int action, int mods) override
+    {
+        if      ((key == GLFW_KEY_UP)    || (key == GLFW_KEY_W)) camera.move_forward(frame_dt);
+        else if ((key == GLFW_KEY_DOWN)  || (key == GLFW_KEY_S)) camera.move_backward(frame_dt);
+        else if ((key == GLFW_KEY_RIGHT) || (key == GLFW_KEY_D)) camera.straight_right(frame_dt);
+        else if ((key == GLFW_KEY_LEFT)  || (key == GLFW_KEY_A)) camera.straight_left(frame_dt);
+
+        if ((key == GLFW_KEY_KP_ADD) && (action == GLFW_RELEASE))
+            hell = !hell;
+    }
+
+    void on_mouse_move() override
+    {
+        double norm = glm::length(mouse_delta);
+        if (norm > 0.01)
+            camera.rotateXY(mouse_delta / norm, norm * frame_dt);
+    }
+};
+
+//=======================================================================================================================================================================================================================
+// program entry point
+//=======================================================================================================================================================================================================================
+
+int main(int argc, char *argv[])
+{
+    //===================================================================================================================================================================================================================
+    // initialize GLFW library
+    // create GLFW window and initialize GLEW library
+    // 4AA samples, OpenGL 3.3 context, screen resolution : 1920 x 1080
+    //===================================================================================================================================================================================================================
+    if (!glfw::init())
+        exit_msg("Failed to initialize GLFW library. Exiting ...");
+
+    demo_window_t window("Ray Marching with Compute Shader", 4, 4, 3, 1920, 1080, true);
+
+    //===================================================================================================================================================================================================================
+    // ray march compute shader
+    //===================================================================================================================================================================================================================
+    glsl_program_t ray_marcher(glsl_shader_t(GL_COMPUTE_SHADER, "glsl/ray_marcher.cs"));
+    ray_marcher.enable();
+    ray_marcher["focal_scale"] = glm::vec2(1.0f / window.camera.projection_matrix[0][0], 1.0f / window.camera.projection_matrix[1][1]);
+    ray_marcher["tb_tex"] = 1;
+    ray_marcher["noise_tex"] = 2;
+    uniform_t uni_rm_camera_matrix = ray_marcher["camera_matrix"];
+    uniform_t uni_rm_hell = ray_marcher["hell"];
+
+    glsl_program_t quad_renderer(glsl_shader_t(GL_VERTEX_SHADER,   "glsl/quad.vs"),
+                                 glsl_shader_t(GL_FRAGMENT_SHADER, "glsl/quad.fs"));
+    quad_renderer.enable();
+    quad_renderer["raymarch_tex"] = 0;
+
+    //===================================================================================================================================================================================================================
+    // create output textures, load texture for trilinear blend shading and generate noise texture
+    //===================================================================================================================================================================================================================
+    glActiveTexture(GL_TEXTURE0);
+    GLuint output_image;
+    glGenTextures(1, &output_image);
+    glBindTexture(GL_TEXTURE_2D, output_image);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA32F, windows.res_x, windows.res_y);
+    glBindImageTexture(0, output_image, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+
+    glActiveTexture(GL_TEXTURE1);
+    GLuint tb_tex_id = image::png::texture2d("../../../resources/tex2d/clay.png");
+
+    glActiveTexture(GL_TEXTURE2);
+    GLuint noise_tex = glsl_noise::randomRGBA_shift_tex256x256(glm::ivec2(37, 17));
+
+
+    //===================================================================================================================================================================================================================
+    // OpenGL rendering parameters setup
+    //===================================================================================================================================================================================================================
+    GLuint vao_id;
+    glGenVertexArrays(1, &vao_id);
+
+    //===================================================================================================================================================================================================================
+    // main program loop : just clear the buffer in a loop
+    //===================================================================================================================================================================================================================
+    while(!window.should_close())
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        window.new_frame();
+
+        glm::mat4 camera_matrix = glm::inverse(window.camera.view_matrix);
+
+        ray_marcher.enable();
+        uni_rm_camera_matrix = camera_matrix;
+        uni_rm_hell = (int) window.hell;
+        glDispatchCompute(windows.res_x, windows.res_y, 1);
+
+        glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+        quad_renderer.enable();
+        glBindVertexArray(vao_id);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+        window.end_frame();
+    }
+
+    //===================================================================================================================================================================================================================
+    // terminate the program and exit
+    //===================================================================================================================================================================================================================
+    glDeleteVertexArrays(1, &vao_id);
+
+    glfw::terminate();
+    return 0;
+}
