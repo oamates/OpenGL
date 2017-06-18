bde5f02cfff9da09d7bf5d372db357b3e2283977
Alexander Foksha <afoksha@gmail.com>
2017-06-17 17:09:37 -0700

    Yet one more class made readable

--- demos/083_CMS/vec3.hpp
+++ demos/083_CMS/vec3.hpp
@@ -1,269 +1,195 @@
-#ifndef CMS_VEC3_H
-#define CMS_VEC3_H
+#ifndef _cms_vec3_included_0145670187568723507125638724508712307213516235620853
+#define _cms_vec3_included_0145670187568723507125638724508712307213516235620853
 
+#include <cmath>
 #include <iostream>
 
 namespace cms
 {
 
-
-/// @brief define EPSILON for floating point comparison
+// define EPSILON for floating point comparison
 #ifndef EPSILON
-  const static float EPSILON = 0.00001f;
+    const static float EPSILON = 0.00001f;
 #endif
 
-
- /// @brief FCompare macro used for floating point comparision functions
-  #define FCompare(a,b) \
-      ( ((a)-EPSILON)<(b) && ((a)+EPSILON)>(b) )
- /// Modified by - George Rassovsky - end ///
+// FCompare macro used for floating point comparision functions
+#define FCompare(a,b) \
+    (((a) - EPSILON) < (b) && ((a) + EPSILON) > (b))
 
 
-
-class Vec3
+struct Vec3
 {
-public:
-  /// @brief Precision of the real for the Vec3 class
-  typedef float Real;
+    float m_x, m_y, m_z;
 
+    Vec3(const Vec3& _v) : m_x(_v.m_x), m_y(_v.m_y), m_z(_v.m_z) {}
+    Vec3(float _x = 0.0, float _y = 0.0, float _z = 0.0) : m_x(_x), m_y(_y), m_z(_z) {}
 
-public:
+    void set(float _x, float _y, float _z)
+    {
+        m_x = _x;
+        m_y = _y;
+        m_z = _z;
+    }
 
-  /// @brief copy ctor
-  /// @param[in] _v the value to set
-  Vec3( const Vec3& _v  ) :
-    m_x(_v.m_x),
-    m_y(_v.m_y),
-    m_z(_v.m_z){;}
+    void set(const Vec3& _v)
+    {
+        m_x = _v.m_x;
+        m_y = _v.m_y;
+        m_z = _v.m_z;
+    }
 
-
-  /// @brief initialise the constructor from 3 or 4 Real
-  /// @param[in]  _x x value
-  /// @param[in]  _y y value
-  /// @param[in]  _z z value
-  /// @param[in]  _w 1.0f default so acts as a points
-  Vec3(Real _x=0.0,  Real _y=0.0, Real _z=0.0  ):
-    m_x(_x),
-    m_y(_y),
-    m_z(_z){;}
-
-
-
-  /// @brief sets the Vec3 component from 3 values
-  /// @param[in]  _x the x component
-  /// @param[in]  _y the y component
-  /// @param[in]  _z the z component
-  void set( Real _x,  Real _y,  Real _z );
+    void set(const Vec3* _v)
+    {
+        m_x = _v->m_x;
+        m_y = _v->m_y;
+        m_z = _v->m_z;
+    }
   
+    float dot(const Vec3& _v) const
+    {
+        return m_x * _v.m_x + m_y * _v.m_y + m_z * _v.m_z;
+    }
 
+    void cross(const Vec3& _v1, const Vec3& _v2 )
+    {
+        m_x = _v1.m_y * _v2.m_z - _v1.m_z * _v2.m_y;
+        m_y = _v1.m_z * _v2.m_x - _v1.m_x * _v2.m_z;
+        m_z = _v1.m_x * _v2.m_y - _v1.m_y * _v2.m_x;
+    }
 
-  /// @brief set from another  Vec3
-  /// @param[in]  _v the Vec3 to set from
-  void set( const Vec3& _v );
+    Vec3 cross(const Vec3& _v )const
+    {
+        return Vec3(
+            m_y * _v.m_z - m_z * _v.m_y,
+            m_z * _v.m_x - m_x * _v.m_z,
+            m_x * _v.m_y - m_y * _v.m_x
+        );
+    }
 
+    void null()
+    {
+        m_x = 0.0f;
+        m_y = 0.0f;
+        m_z = 0.0f;
+    }
+    
+    float& operator[] (const int& _i)
+    {
+        return (&m_x)[_i];
+    }
 
+    float length() const
+        { return sqrt(m_x * m_x + m_y * m_y + m_z * m_z); }
 
-  /// @brief set from another  Vec3
-  /// @param[in]  _v the Vec3 to set from
-  void set( const Vec3* _v );
+    float lengthSquared() const
+        { return m_x * m_x + m_y * m_y + m_z * m_z; }
+
+    void normalize()
+    {
+        float l = sqrt(m_x * m_x + m_y * m_y + m_z * m_z);
+        m_x /= l;
+        m_y /= l;
+        m_z /= l;
+    }
   
-
-
-  /// @brief return this dotted with _b
-  /// @param[in]  _b vector to dot current vector with
-  /// @returns  the dot product
-  Real dot( const Vec3 &_b  )const;
-
-
-
-  /// @brief set the Vec3 as the cross product from 2 other Vec3
-  /// @param[in]  _v1 the first vector
-  /// @param[in]  _v2 the second vector
-  void cross(const Vec3& _v1, const Vec3& _v2 );
+    float inner(const Vec3& _v) const
+        { return m_x * _v.m_x + m_y * _v.m_y + m_z * _v.m_z; }
   
-
-
-  /// @brief return the cross product of this cross with b
-  /// @param[in]  _b the vector cross this with
-  /// @returns  the result of this cross b
-  Vec3 cross(const Vec3& _b )const;
-
-
-
-  /// @brief clears the Vec3 to 0,0,0
-  void null();
+    Vec3 outer(const Vec3& _v) const
+    {
+        float x = (m_y * _v.m_z) - (m_z * _v.m_y);
+        float y = (m_z * _v.m_x) - (m_x * _v.m_z);
+        float z = (m_x * _v.m_y) - (m_y * _v.m_x);
+        return Vec3(x, y, z);
+    }
   
+    void operator += (const Vec3& _v)
+    {
+        m_x += _v.m_x;
+        m_y += _v.m_y;
+        m_z += _v.m_z;
+    }
 
+    void operator -= (const Vec3& _v)
+    {
+        m_x -= _v.m_x;
+        m_y -= _v.m_y;
+        m_z -= _v.m_z;
+    }
 
-  /// @brief [] index operator to access the index component of the Vec3
-  /// @returns  this[x] as a Real
-  Real& operator[]( const int& _i ) ;
+    Vec3 operator * (float _i) const
+        { return Vec3(m_x * _i, m_y * _i, m_z * _i); }
 
+    Vec3 operator + (const Vec3 &_v) const
+        { return Vec3(m_x + _v.m_x, m_y + _v.m_y, m_z + _v.m_z); }
 
+    Vec3 operator / (float _v) const
+        { return Vec3(m_x / _v, m_y / _v, m_z / _v); }
 
-  /// @brief Normalize the vector using
-  /// \n \f$x=x/\sqrt{x^2+y^2+z^2} \f$
-  /// \n \f$y=y/\sqrt{x^2+y^2+z^2} \f$
-  /// \n \f$z=z/\sqrt{x^2+y^2+z^2} \f$
-  void normalize();
+    Vec3 operator / (const Vec3& _v) const
+        { return Vec3(m_x / _v.m_x, m_y / _v.m_y, m_z / _v.m_z); }
+
+    void operator /= (float _v)
+    {
+        m_x /= _v;
+        m_y /= _v;
+        m_z /= _v;
+    }
+
+    void operator *= (float _v)
+    {
+        m_x *= _v;
+        m_y *= _v;
+        m_z *= _v;
+    }
   
+    Vec3 operator - (const Vec3 &_v) const
+        { return Vec3(m_x - _v.m_x, m_y - _v.m_y, m_z - _v.m_z); }
 
+    Vec3 operator - () const
+        { return Vec3(-m_x, -m_y, -m_z); }
 
-  /// @brief calculate the inner product of this vector and vector passed in
-  /// @param[in] _v the vector to calculate inner product with
-  /// @returns the inner product
-  Real inner( const Vec3& _v )const;
+    Vec3 operator * (const Vec3 &_v) const
+        { return Vec3(m_x * _v.m_x, m_y * _v.m_y, m_z * _v.m_z); }
+
+    Vec3& operator = (const Vec3 &_v)
+    {
+        m_x = _v.m_x;
+        m_y = _v.m_y;
+        m_z = _v.m_z;
+        return *this;
+    }
+
+    Vec3 &operator = (float _v)
+    {
+        m_x = _v;
+        m_y = _v;
+        m_z = _v;
+        return *this;
+    }
+
+    bool operator == (const Vec3 &_v) const
+    {
+        return FCompare(_v.m_x, m_x) && FCompare(_v.m_y, m_y) && FCompare(_v.m_z, m_z);
+    }
   
+    bool operator != (const Vec3 &_v) const
+    {
+        return !FCompare(_v.m_x,m_x) || !FCompare(_v.m_y,m_y) || !FCompare(_v.m_z,m_z);
+    }
 
-
-  /// @brief compute the outer product of this vector and vector
-  /// @param[in] _v the vector to calc against
-  /// @returns a new vector
-  Vec3 outer( const Vec3& _v )const;
-  
-
-
-  /// @brief returns the length of the vector
-  /// @returns  \f$\sqrt{x^2+y^2+z^2} \f$
-  Real length() const;
-  
-
-
-  /// @brief returns the length squared of the vector (no sqrt so quicker)
-  /// @returns  \f$x^2+y^2+z^2 \f$
-  Real lengthSquared() const;
-
-
-
-  /// @brief += operator add Vec3 v to current Vec3
-  /// @param[in]  &_v Vec3 to add
-  void operator+=(const Vec3& _v );
-
-
-
-  /// @brief -= operator this-=v
-  /// @param[in]  &_v Vec3 to subtract
-  void operator-=( const Vec3& _v );
-
-
-
-  /// @brief this * i for each element
-  /// @param[in]  _i the scalar to mult by
-  /// @returns Vec3
-  Vec3 operator *( Real _i )const;
-
-
-
-  /// @brief + operator add Vec3+Vec3
-  /// @param[in]  &_v the value to add
-  /// @returns the Vec3 + v
-  Vec3 operator +(const Vec3 &_v )const;
-
-
-
-  /// @brief divide Vec3 components by a scalar
-  /// @param[in] _v the scalar to divide by
-  /// @returns a Vec3 V(x/v,y/v,z/v,w)
-  Vec3 operator/( Real _v )const;
-
-
-
-  /// @brief operator div Vec3/Vec3
-  /// @param[in]  _v the value to div by
-  /// @returns Vec3 / Vec3
-  Vec3 operator/( const Vec3& _v )const;
-
-
-
-  /// @brief divide this Vec3 components by a scalar
-  /// @param[in] _v the scalar to divide by
-  /// sets the Vec3 to Vec3 V(x/v,y/v,z/v,w)
-  void operator/=( Real _v );
-  
-
-
-  /// @brief multiply this Vec3 components by a scalar
-  /// @param[in] _v the scalar to multiply by
-  /// sets the Vec3 to Vec3 V(x*v,y*v,z*v,w)
-  void operator*=( Real _v );
-  
-
-
-  /// @brief subtraction operator subtract vevtor-Vec3
-  /// @param[in]  &_v the value to sub
-  /// @returns this - v
-  Vec3 operator-( const Vec3 &_v )const;
-
-
-
-  /// @brief negate the Vec3 components
-  Vec3 operator-() const;
-
-
-
-  /// @brief * operator mult vevtor*Vec3
-  /// @param[in]  _v the value to mult
-  /// @returns new Vec3 this*v
-  Vec3 operator*( const Vec3 &_v )const;
-
-
-
-  /// @brief assignment operator set the current Vec3 to rhs
-  /// @param[in] _v the Vec3 to set
-  /// @returns a new Vec3
-  Vec3 &operator =( const Vec3 &_v );
-
-
-
-  /// @brief assignment operator set the current Vec3 to rhs
-  /// @param[in] _v the Real to set
-  /// @returns a new Vec3
-  Vec3 &operator =(Real _v );
-
-
-
-  /// @brief check for equality uses FCompare (from Util.h) as Real values
-  /// @param[in] _v the Vec3 to check against
-  /// @returns true or false
-  bool operator==( const Vec3 &_v )const;
-  
-
-
-  /// @brief not equal check
-  /// @param[in] _v the Vec3 to check against
-  /// @returns true of false
-  bool operator!=( const Vec3 &_v )const;
-
-public :
-
-  /// @brief x component of the Vec3
-  Real m_x;
-
-
-  /// @brief y component of the Vec3
-  Real m_y;
-
-
-  /// @brief z component of the Vec3
-  Real m_z;
 };
 
+inline std::ostream& operator << (std::ostream& _output, const Vec3& _v)
+{
+    return _output << "[" << _v.m_x << "," << _v.m_y << "," << _v.m_z << "]";
+}
 
+inline std::istream& operator >> (std::istream& _input, Vec3 &_s)
+{
+    return _input >> _s.m_x >> _s.m_y >> _s.m_z;
+}
 
+} // namespace cms
 
-/// @brief insertion operator to print out the Vec3
-/// @param[in] _output the stream to write to
-/// @param[in] _s the Vec3 to write
-std::ostream& operator<<( std::ostream& _output, const Vec3& _s );
- 
-
-
-/// @brief extraction operator to read in  the Vec3
-/// @param[in] _input the stream read from
-/// @param[in] _s the Vec3 to write
-std::istream& operator>>( std::istream& _input, Vec3 &_s );
-
-
-} //namespace cms
-
-#endif //CMS_VEC3_H
+#endif // _cms_vec3_included_0145670187568723507125638724508712307213516235620853
