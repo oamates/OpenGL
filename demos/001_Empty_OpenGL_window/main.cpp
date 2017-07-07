2e26304ed73c832cad6ec1f3ab950639cc179e19
Alexander Foksha <afoksha@luxoft.com>
2017-07-07 03:30:50 +0300

    Brand new shader compiles

--- demos/001_Empty_OpenGL_window/main.cpp
+++ demos/001_Empty_OpenGL_window/main.cpp
@@ -4,7 +4,6 @@
 #include "log.hpp"
 #include "gl_info.hpp"
 #include "glfw_window.hpp"
-#include "shader.hpp"
 
 //=======================================================================================================================================================================================================================
 // program entry point
@@ -25,114 +24,6 @@ int main(int argc, char *argv[])
     // OpenGL rendering parameters setup : background color -- dark blue
     //===================================================================================================================================================================================================================
     glClearColor(0.01f, 0.0f, 0.08f, 0.0f);
-
-
-    glsl_program_t compute_shader = glsl_program_t(glsl_shader_t(GL_COMPUTE_SHADER, "glsl/sdf_compute.cs"));
-    uniform_t uni_compute_value = compute_shader["value"];
-
-    glsl_program_t combine_shader = glsl_program_t(glsl_shader_t(GL_COMPUTE_SHADER, "glsl/sdf_combine.cs"));
-    uniform_t uni_combine_sigma = combine_shader["sigma"];
-
-
-    const int size = 256;
-
-
-    GLuint uint32_texture_id1, uint32_texture_id2;
-
-    glActiveTexture(GL_TEXTURE1);
-    glGenTextures(1, &uint32_texture_id1);
-    glBindTexture(GL_TEXTURE_3D, uint32_texture_id1);
-
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-    glTexStorage3D(GL_TEXTURE_3D, 1, GL_R32UI, 256, 256, 256);
-
-
-    compute_shader.enable();
-    uni_compute_value = (unsigned int) 1024;
-    glBindImageTexture(0, uint32_texture_id1, 0, GL_TRUE, 0, GL_WRITE_ONLY, GL_R32UI);
-    glDispatchCompute(32, 32, 32);
-
-
-    glActiveTexture(GL_TEXTURE2);
-    glGenTextures(1, &uint32_texture_id2);
-    glBindTexture(GL_TEXTURE_3D, uint32_texture_id2);
-
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-    glTexStorage3D(GL_TEXTURE_3D, 1, GL_R32F, 256, 256, 256);
-
-    uni_compute_value = (unsigned int) 512;
-    glBindImageTexture(0, uint32_texture_id2, 0, GL_TRUE, 0, GL_WRITE_ONLY, GL_R32UI);
-    glDispatchCompute(32, 32, 32);
-
-
-
-
-
-    GLuint texture_id;
-
-    glActiveTexture(GL_TEXTURE0);
-    glGenTextures(1, &texture_id);
-    glBindTexture(GL_TEXTURE_3D, texture_id);
-
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-    glTexStorage3D(GL_TEXTURE_3D, 1, GL_R32F, 256, 256, 256);
-
-    combine_shader.enable();
-    glBindImageTexture(0, texture_id, 0, GL_TRUE, 0, GL_WRITE_ONLY, GL_R32F);
-    glBindImageTexture(1, uint32_texture_id1, 0, GL_TRUE, 0, GL_READ_ONLY, GL_R32UI);
-    glBindImageTexture(2, uint32_texture_id2, 0, GL_TRUE, 0, GL_READ_ONLY, GL_R32UI);
-
-    glDispatchCompute(32, 32, 32);
-    glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
-
-
-    GLuint pixel_data_size = 4 * 256 * 256 * 256;
-    GLvoid* pixels = (GLvoid*) malloc(pixel_data_size);
-
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_3D, texture_id);
-    glGetTexImage(GL_TEXTURE_3D, 0, GL_RED, GL_FLOAT, pixels);
-
-    debug_msg("Printing float texture");
-
-    int w = 112;
-
-    for (int i0 = w; i0 < 256 - w; ++i0) for (int i1 = w; i1 < 256 - w; ++i1) for (int i2 = w; i2 < 256 - w; ++i2)
-    {
-        int index = i0 + 256 * i1 + 65536 * i2;
-        GLfloat value = ((GLfloat *) pixels)[index];
-        debug_msg("texture[%u, %u, %u] = %f.", i0, i1, i2, value);
-    }
-
-    free(pixels);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     //===================================================================================================================================================================================================================
     // main program loop : just clear the color and depth buffer in a loop
--- demos/058_SDF_Texture3D/glsl/udf_compute.cs
+++ demos/058_SDF_Texture3D/glsl/udf_compute.cs
@@ -2,19 +2,35 @@
 
 layout (local_size_x = 256, local_size_y = 1, local_size_z = 1) in;
 
+const int n = 8;
+const int p2 = 1 << n;
+const int p2m1 = 1 << (n - 1);
+const float inv_p2 = 1.0 / p2;
+const float inv_p2m1 = 1.0 / p2m1;
+
+//==============================================================================================================================================================
+// input :: the number of triangles to process and ...
+//   image unit 0 :: index buffer of GL_TRIANGLES type
+//   image unit 1 :: positions buffer, vec3 with w component ignored for now
+//==============================================================================================================================================================
 uniform uint triangles;
 
-//==============================================================================================================================================================
-// input  :: 
-//   image unit 0 :: index buffer of GL_TRIANGLES type
-//   image unit 1 :: positions buffer, vec3 with w component irrelevant
-//==============================================================================================================================================================
-
-layout (rgba32i, binding = 0) uniform iimageBuffer index_buffer;
+layout (r32ui, binding = 0) uniform uimageBuffer index_buffer;
 layout (rgba32f, binding = 1) uniform imageBuffer vertex_buffer;
 
+//==============================================================================================================================================================
+// auxiliary buffer needed for search octree construction
+// if the output texture is 2^{n} x 2^{n} x 2^{n}, then this buffer should be of
+// the size 8 + 64 + ... + 2^{3n-3}
+// and atomic counter backed up by an atomic counter buffer for triangle separation among execution threads 
+//==============================================================================================================================================================
+layout (r32ui, binding = 2) uniform uimageBuffer octree;
+layout (binding = 0, offset = 0) uniform atomic_uint triangle_index;
+
+//==============================================================================================================================================================
+// output 3d texture, of size 
+//==============================================================================================================================================================
 layout (r32ui, binding = 0) uniform uimage3D udf_tex;
-layout (binding = 0, offset = 0) uniform atomic_int index_cntr;
 
 //==============================================================================================================================================================
 // unsigned distance from point to triangle function
@@ -24,6 +40,7 @@ layout (binding = 0, offset = 0) uniform atomic_int index_cntr;
 float dot2(vec3 v) 
     { return dot(v, v); }
 
+/*
 float triangle_udf(vec3 p, vec3 a, vec3 b, vec3 c)
 {
     vec3 AB = b - a; vec3 pa = p - a;
@@ -48,64 +65,201 @@ float triangle_udf(vec3 p, vec3 a, vec3 b, vec3 c)
         )
     );
 }
+*/
 
-const int max_level = 8;
+const vec3 shift[8] = vec3[]
+(
+    vec3(-1.0, -1.0, -1.0),
+    vec3( 1.0, -1.0, -1.0),
+    vec3(-1.0,  1.0, -1.0),
+    vec3( 1.0,  1.0, -1.0),
+    vec3(-1.0, -1.0,  1.0),
+    vec3( 1.0, -1.0,  1.0),
+    vec3(-1.0,  1.0,  1.0),
+    vec3( 1.0,  1.0,  1.0)
+);
+
+const float INTEGRAL_SCALE = 268435456.0;
+const float INV_INT_SCALE = 1.0 / INTEGRAL_SCALE;
+
+const float sqrt3 = 1.73205080757;
+
+float node_diameter[n] = float []
+(
+    (2.000000 - inv_p2m1) * sqrt3,
+    (1.000000 - inv_p2m1) * sqrt3, 
+    (0.500000 - inv_p2m1) * sqrt3, 
+    (0.250000 - inv_p2m1) * sqrt3,
+    (0.125000 - inv_p2m1) * sqrt3, 
+    (0.062500 - inv_p2m1) * sqrt3, 
+    (0.031250 - inv_p2m1) * sqrt3, 
+    (0.015625 - inv_p2m1) * sqrt3
+);
+
+
 
 void main()
 {
     //==========================================================================================================================================================
     // get the index of the triangle this invocation will work on 
     //==========================================================================================================================================================
-    int head = atomicCounterIncrement(index_cntr);
+    uint triangle = atomicCounterIncrement(triangle_index);
 
-    while (head < triangles)
+    while (triangle < triangles)
     {
         //======================================================================================================================================================
         // get the indices and the vertices of the triangle
         //======================================================================================================================================================
-        int base_index = 3 * head;
+        int base_index = 3 * int(triangle);
 
-        int iA = imageLoad(index_buffer, base_index + 0);         
-        int iB = imageLoad(index_buffer, base_index + 1);
-        int iC = imageLoad(index_buffer, base_index + 2);
+        uint iA = imageLoad(index_buffer, base_index + 0).x;        
+        uint iB = imageLoad(index_buffer, base_index + 1).x;
+        uint iC = imageLoad(index_buffer, base_index + 2).x;
 
-        vec3 vA = imageLoad(vertex_buffer, iA).xyz;
-        vec3 vB = imageLoad(vertex_buffer, iB).xyz;
-        vec3 vC = imageLoad(vertex_buffer, iC).xyz;
+        vec3 vA = imageLoad(vertex_buffer, int(iA)).xyz;
+        vec3 vB = imageLoad(vertex_buffer, int(iB)).xyz;
+        vec3 vC = imageLoad(vertex_buffer, int(iC)).xyz;
 
         //======================================================================================================================================================
         // calculate triangle diameter
         //======================================================================================================================================================
-        vec3 BA = vB - vA; float lBA = length(BA);
-        vec3 CB = vC - vB; float lCB = length(CB);
-        vec3 AC = vA - vC; float lAC = length(AC);
 
-        float diameter = max(max(lBA, lCB), lAC);
+        vec3 BA = vB - vA; float dBA = dot2(BA);
+        vec3 CB = vC - vB; float dCB = dot2(CB);
+        vec3 AC = vA - vC; float dAC = dot2(AC);
 
+        float triangle_diameter = max(max(sqrt(dBA), sqrt(dCB)), sqrt(dAC));
 
-        int digits[max_level] = {0, 0, 0, 0, 0, 0, 0, 0};
+        vec3 normal = cross(BA, AC);
+        float inv_area = length(normal);
 
-        ivec3 Pi[max_level];
-        Pi[0] = ivec3(128);
+        //======================================================================================================================================================
+        // our position in the octree and corresponding index into octree buffer
+        //======================================================================================================================================================
+        int octree_digit[n] = {0, 0, 0, 0, 0, 0, 0, 0};
+        int node_index = 0;
 
+        //======================================================================================================================================================
+        // the algorithm starts with jumping from level 0 to level 1 and recursively going down/up
+        // when we come back to the level 0, distance octree will be traversed and updated
+        //======================================================================================================================================================
         int level = 1;
+        float scale = 0.5;
+
+        vec3 node_position[n];
+        node_position[0] = vec3(0.0);
 
         while(level != 0)
         {
-            if (digit[level] < 8)
+            //==================================================================================================================================================
+            // update the current position of the node and calculate the distance from the triangle to it
+            //==================================================================================================================================================
+            node_position[level] = node_position[level - 1] + scale * shift[octree_digit[level]];
+            vec3 p = node_position[level];
+
+            vec3 pA = p - vA;
+            vec3 pB = p - vB;
+            vec3 pC = p - vC;
+
+            float q = sign(dot(cross(BA, normal), pA)) + sign(dot(cross(CB, normal), pB)) + sign(dot(cross(AC, normal), pC));
+
+            float distance_to_node = (q >= 2.0f) ? inv_area * abs(dot(normal, pA)) : 
+                    sqrt(
+                        min(
+                            min(
+                                dot2(BA * clamp(dot(BA, pA) / dBA, 0.0f, 1.0f) - pA),
+                                dot2(CB * clamp(dot(CB, pB) / dCB, 0.0f, 1.0f) - pB)
+                            ), 
+                                dot2(AC * clamp(dot(AC, pC) / dAC, 0.0f, 1.0f) - pC)
+                        )
+                    );
+
+            uint idistance_to_node = uint(distance_to_node * INTEGRAL_SCALE);
+
+            uint icurrent_distance = imageAtomicMin(octree, node_index, idistance_to_node);
+            float current_distance = float(icurrent_distance) * INV_INT_SCALE;
+
+            //==================================================================================================================================================
+            // compare the distance with the distance currently stored in octree
+            //==================================================================================================================================================
+            if (distance_to_node >= node_diameter[level] + current_distance + triangle_diameter)
             {
-
+                //==================================================================================================================================================
+                // current_distance is small enough, the node can be skipped completely
+                // either stay on the same level or go up if digit[level] == 7
+                //==================================================================================================================================================
+                while(octree_digit[level] == 7)
+                {
+                    scale += scale;
+                    node_index = (node_index >> 3) - 1;
+                    level--;
+                }
+                octree_digit[level]++;
+                node_index++;
             }
-            ivec3 Pi = 
-            for(int )
+            else
+            {
+                if (level == n - 1)
+                {
+                    //==============================================================================================================================================
+                    // we came to 8 octree leafs, compute the 8 distances and do atomicMin
+                    //==============================================================================================================================================
+                    vec3 leaf_node = node_position[n - 1];
+                    for(int v = 0; v < 8; ++v)
+                    {
+                        vec3 leaf_position = leaf_node + inv_p2 * shift[v];
+                        ivec3 uvw = ivec3(floor(128.0 + 128.0 * leaf_position));
 
+                        vec3 pA = leaf_position - vA;
+                        vec3 pB = leaf_position - vB;
+                        vec3 pC = leaf_position - vC;
+
+                        float q = sign(dot(cross(BA, normal), pA)) + sign(dot(cross(CB, normal), pB)) + sign(dot(cross(AC, normal), pC));
+                        float distance_to_leaf = (q >= 2.0f) ? inv_area * abs(dot(normal, pA)) : 
+                            sqrt(
+                                min(
+                                    min(
+                                        dot2(BA * clamp(dBA / dot2(BA), 0.0f, 1.0f) - pA),
+                                        dot2(CB * clamp(dCB / dot2(CB), 0.0f, 1.0f) - pB)
+                                    ), 
+                                        dot2(AC * clamp(dAC / dot2(AC), 0.0f, 1.0f) - pC)
+                                )
+                            );
+
+                        uint idistance_to_leaf = uint(distance_to_leaf * INTEGRAL_SCALE);
+
+                        imageAtomicMin(udf_tex, uvw, idistance_to_leaf);
+
+                    }
+
+                    //==============================================================================================================================================
+                    // come back one/more levels up in the octree
+                    //==============================================================================================================================================
+                    while(octree_digit[level] == 7)
+                    {
+                        scale += scale;
+                        node_index = (node_index >> 3) - 1;
+                        level--;
+                    }
+                    octree_digit[level]++;
+                    node_index++;
+                }
+                else
+                {
+                    //==============================================================================================================================================
+                    // go down
+                    //==============================================================================================================================================
+                    level++;
+                    octree_digit[level] = 0;
+                    node_index = (node_index + 1) << 3;
+                    scale *= 0.5;
+                }
+            }
         }
 
-
-
         //======================================================================================================================================================
-        // done! proceed to next triangle
+        // done ... proceed to next triangle
         //======================================================================================================================================================
-        head = atomicCounterIncrement(index);
+        triangle = atomicCounterIncrement(triangle_index);
     }
 }
